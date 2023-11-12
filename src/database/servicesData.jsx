3876070039c63e4f410ea41c23882453f5e57fad
Farid Rashidov <farikrashidov@gmail.com>
2023-11-13 00:39:09 +0400

    Moving images to public folder

--- src/database/servicesData.jsx
+++ src/database/servicesData.jsx
@@ -4,61 +4,61 @@ export const servicesData = [
   {
     id: 1,
     title: "Kommunal xidmət",
-    image: "../src/images/communal-service.svg",
+    image: "../../public/images/agency-service.svg",
   },
   {
     id: 2,
     title: "Mobil operatorlar",
-    image: "../src/images/mobile-service.svg",
+    image: "../../public/images/mobile-service.svg",
   },
   {
     id: 3,
     title: "Dövlət ödənişləri",
-    image: "../src/images/gover-service.svg",
+    image: "../../public/images/gover-service.svg",
   },
   {
     id: 4,
     title: "Bank xidmətləri",
-    image: "../src/images/bank-service.svg",
+    image: "../../public/images/bank-service.svg",
   },
   {
     id: 5,
     title: "İnternet",
-    image: "../src/images/internet-service.svg",
+    image: "../../public/images/internet-service.svg",
   },
   {
     id: 6,
     title: "Kabel TV",
-    image: "../src/images/kabeltv-service.svg",
+    image: "../../public/images/kabeltv-service.svg",
   },
   {
     id: 7,
     title: "Telefon",
-    image: "../src/images/mobile-service.svg",
+    image: "../../public/images/mobile-service.svg",
   },
   {
     id: 8,
     title: "Elektron pul kisəsi",
-    image: "../src/images/ewallet-service.svg",
+    image: "../../public/images/ewallet-service.svg",
   },
   {
     id: 9,
     title: "Sığorta",
-    image: "../src/images/insurance-service.svg",
+    image: "../../public/images/insurance-service.svg",
   },
   {
     id: 10,
     title: "Agent şəbəkəsi",
-    image: "../src/images/agency-service.svg",
+    image: "../../public/images/agency-service.svg",
   },
   {
     id: 11,
     title: "Əyləncə",
-    image: "../src/images/fun-service.svg",
+    image: "../../public/images/fun-service.svg",
   },
   {
     id: 12,
     title: "Təhsil",
-    image: "../src/images/edu-service.svg",
+    image: "../../public/images/edu-service.svg",
   },
 ];
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
