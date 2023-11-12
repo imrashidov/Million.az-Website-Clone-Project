7bf9d5b85eb79b40029878d5fbd123aa3c9e9629
Farid Rashidov <farikrashidov@gmail.com>
2023-11-12 22:47:10 +0400

    Update

--- src/database/servicesData.jsx
+++ src/database/servicesData.jsx
@@ -4,7 +4,7 @@ export const servicesData = [
   {
     id: 1,
     title: "Kommunal xidmət",
-    image: "../src/images/communal-service.svg",
+    image: require("../src/images/communal-service.svg"),
   },
   {
     id: 2,
